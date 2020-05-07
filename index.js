c3368f276497c1d079e876b1a61f6f7ebee677bf
Erik Ford <ekford18@gmail.com>
2020-05-06 17:25:45 -0700

    completed errors

--- index.js
+++ index.js
@@ -19,15 +19,31 @@ app.use(cors());
 //req.params for one search / :id `this.props.match.params.whatever` in react router
 
 app.get('/location', async(request, response) => {
-    const data = await superagent.get(`https://us1.locationiq.com/v1/search.php?key=${process.env.LOCATION_IQ_KEY}&q=${request.query.search}&format=json`);
-    const transformedLocationData = transformedLocation(data.body);
-    response.json(transformedLocationData);
+    try {
+        const data = await superagent.get(`https://us1.locationiq.com/v1/search.php?key=${process.env.LOCATION_IQ_KEY}&q=${request.query.search}&format=json`);
+        const transformedLocationData = transformedLocation(data.body);
+        response.json(transformedLocationData);
+    } catch (e) {
+        console.error(e);
+        response.json({
+            status: 404,
+            responseText: `You've come too far fool!`,
+        });
+    }
 });
 
 app.get('/weather', async(request, response) => {
-    const data = await superagent.get(`https://api.weatherbit.io/v2.0/forecast/daily?&lat=${request.query.latitude}&lon=${request.query.longitude}&key=${process.env.WEATHER_KEY}`);
-    const transformedWeatherData = transformedWeather(data.body);
-    response.json(transformedWeatherData);
+    try {
+        const data = await superagent.get(`https://api.weatherbit.io/v2.0/forecast/daily?&lat=${request.query.latitude}&lon=${request.query.longitude}&key=${process.env.WEATHER_KEY}`);
+        const transformedWeatherData = transformedWeather(data.body);
+        response.json(transformedWeatherData);
+    } catch (e) {
+        console.error(e);
+        response.json({
+            status: 404,
+            responseText: `You've come too far fool!`,
+        });
+    }
 });
 
 app.listen(PORT, () => console.log(`running on port ${PORT}`));
